39540d7f834ba4bc1c993cadc920bef0a460c48a
Madeline Silva <mad.silva.ic@gmail.com>
2025-10-06 18:22:20 -0400

    add new type for overall game and rename old game into uno match

--- app/game/types/Card.ts
+++ app/game/types/Card.ts
@@ -1,3 +1,4 @@
+// An Uno card as it exists in the deck or a player's hand
 export type CardColor = 'red' | 'blue' | 'green' | 'yellow'| 'black';
 export type CardType = 'number' | 'skip' | 'reverse' | 'draw2' | 'wild' | 'wildDraw4';
 
--- app/game/types/Game.ts
+++ app/game/types/Game.ts
@@ -1,16 +1,11 @@
+// A single run of our rogue-like game, consisting of a series of Uno matches
+import type { Modifier } from './Modifier'
 import type Player from './Player'
-import type { Card, CardColor } from './Card'
+import type { UnoMatch } from './UnoMatch'
 
-export interface GameState {
-  players: Player[];
-  currentPlayerIndex: number;
-  turnDirection: 1 | -1;
-  drawPile: Card[];
-  discardPile: Card[];
-  currentColor: CardColor | null;
-  gameStarted: boolean;
-  gameOver: boolean;
-  winner: Player | null;
-  lastAction: string;
-  waitingForWildColor: boolean;
+export interface Game {
+  userPlayer: Player
+  matches: UnoMatch[]
+  currentScreen: 'match' | 'chooseModifier' | 'gameOver'
+  modifiers: Modifier[]
 }
