aeb0986ea947574eaf91b5d621517dc4e3f40951
Madeline Silva <mad.silva.ic@gmail.com>
2025-10-07 14:28:59 -0400

    finish skeleton of play card function

--- app/game/gameLogicInterface.ts
+++ app/game/gameLogicInterface.ts
@@ -26,5 +26,5 @@ export interface GameLogicInterface {
   getPlayers(): Player[];
 
   // Get current player in the current match
-  getCurrentPlayer(): Player
+  getCurrentPlayer(game: Game): Player
 }
