9ee50b51045d86fb43e73d0cd644cd2e378ecdc6
alakn-eng <narayanan.aartil@gmail.com>
2025-10-10 11:53:58 -0400

    added fn to gamelogic and calls to animation for two cards, along with AI player call in AIPlayer

--- app/UserInterface/AIPlayer.tsx
+++ app/UserInterface/AIPlayer.tsx
@@ -1,13 +1,14 @@
 import type Player from "~/game/types/Player"
+import { PlayerHeader } from "./Animation";
 
 interface AIPlayerProps {
   player: Player
 }
 
-export function AIPlayer({player}: AIPlayerProps) {
+export function AIPlayer({ player }: { player: Player }) {
   return (
-    <div className="w-16 h-16 bg-blue-500 text-white flex items-center justify-center rounded-full">
-      hi I'm {player.name}
+    <div className="flex items-center justify-center">
+      <PlayerHeader playerId={player.id} playerName={player.name} />
     </div>
-  )
+  );
 }
